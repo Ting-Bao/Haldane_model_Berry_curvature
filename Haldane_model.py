1519618e435c098b720b4f30dea605eb4b9d0bc6
Ting BAO <bao-ting@foxmail.com>
2022-08-10 11:00:43 +0800

    debug

--- Haldane_model.py
+++ Haldane_model.py
@@ -8,22 +8,41 @@ A training task on Haldane model:
 3. Calculate the sgn(M)-depedent Chern number
 '''
 
+
 from math import cos, e, pi, sqrt
-from os import pathsep
+from xmlrpc.client import FastMarshaller
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from tqdm import tqdm
 
+npoints=100
+d_step=1e-10
+klength=4*pi/sqrt(3)
+step=12.05/npoints
+# lattice vector
+
+# NN: neareat neighbour, mark each element a_i 
+# NNN: next nearest neighbour, mark each element b_i
+NN = np.array([[0.5,sqrt(3)/6],[-0.5,sqrt(3)/6],[0.,-sqrt(3)/3]])
+NNN = np.array([[-0.5,sqrt(3)/2],[-0.5,-sqrt(3)/2],[1.,0.]])
+
+# !!!!!!!!!!!!!!!!!!!!
+#  the d_size should be normalized!
+# !!!!!!!!!!!!!!!!!!!!
+
+dsize=(step**2)/(sqrt(3)/2*(klength**2))
+V=sqrt(3)/2
+dsize=step**2/(((2*pi)**2)/V)
+dsize=(step**2)/(2*pi/V*np.linalg.norm(np.cross([-0.5,sqrt(3)/2,0],[-0.5,-sqrt(3)/2,0]),ord=2))
+print(np.cross([-0.5,sqrt(3)/2,0],[-0.5,-sqrt(3)/2,0]))
+print(np.linalg.norm(2*pi/V*np.cross([-0.5,sqrt(3)/2,0],[-0.5,-sqrt(3)/2,0])))
 def get_H(t1,t2,phi,k,M):
     '''t1 for NN hopping, 
     t2 for NNN hopping,
     phi for phase in NNN hopping,
     return Hamiltonian matrix as k=[kx,ky] under given parameter
     '''
-    # NN: neareat neighbour, mark each element a_i 
-    # NNN: next nearest neighbour, mark each element b_i
-    NN = np.array([[0.5,sqrt(3)/6],[-0.5,sqrt(3)/6],[0.,-sqrt(3)/3]])
-    NNN = np.array([[-0.5,sqrt(3)/2],[-0.5,-sqrt(3)/2],[1.,0.]])
 
     H11=2*t2*(sum([cos(phi+np.dot(k,bi)) for bi in NNN]))+M
     H12=t1*(sum([e**(-1j*np.dot(k,ai)) for ai in NN]))
@@ -33,18 +52,20 @@ def get_H(t1,t2,phi,k,M):
     H=np.array([[H11,H12],[H21,H22]])
     return H
 
-def get_berryphase(kxlist,kylist,berry_curv,dsize=0.05*0.05):
+
+def get_berryphase(kxlist,kylist,berry_curv,dsize=dsize):
     """ intergrate in a rectangle area to get the Berry phase 
     dsize means the step using in meshing the BZ
     the length of recipical lattice vector is 4*pi/sqrt(3)
     """
-    klength=4*pi/sqrt(3)
+
     phase=0.
     for i in range(len(kxlist)):
         kx=kxlist[i]
         ky=kylist[i]
-        if kx>=0 and kx<klength*sqrt(3)/2 and ky>=-(1/sqrt(3))*kx and ky<-(1/sqrt(3))*kx+klength:
-            phase+=dsize*berry_curv[i]
+        if kx>=0 and kx<klength*sqrt(3)/2:
+            if ky>=(1/sqrt(3))*kx and ky<(1/sqrt(3))*kx+klength:
+                phase=phase+dsize*berry_curv[i]
     print(phase)
     return phase
 
@@ -54,8 +75,8 @@ def main(t1,t2,M,phi):
     '''
 
     # do k-mesh
-    sample_kx=np.arange(-8.,8.1,0.05)
-    sample_ky=np.arange(-8.,8.1,0.05)
+    sample_kx=np.linspace(0,12.05,npoints,endpoint=False)
+    sample_ky=np.linspace(0,12.05,npoints,endpoint=False)
     xx,yy=np.meshgrid(sample_kx,sample_ky)
     kxlist=xx.flatten()
     kylist=yy.flatten()
@@ -65,50 +86,45 @@ def main(t1,t2,M,phi):
     berry_curv_2=[]
     berry_curv_total=[]
 
-    for i in range(len(kxlist)):
+    for i in tqdm(range(len(kxlist))):
         kx=kxlist[i]
         ky=kylist[i]
         H=get_H(t1=t1,t2=t2,phi=phi,k=[kx,ky],M=M)
-        Hx=get_H(t1=t1,t2=t2,phi=phi,k=[kx+0.001,ky],M=M)
-        Hy=get_H(t1=t1,t2=t2,phi=phi,k=[kx,ky+0.001],M=M)
-        H_dx = (Hx-H)/0.001# partial H/partial x
-        H_dy = (Hy-H)/0.001# partial H/partial y
+        Hx=get_H(t1=t1,t2=t2,phi=phi,k=[kx+d_step,ky],M=M)
+        Hy=get_H(t1=t1,t2=t2,phi=phi,k=[kx,ky+d_step],M=M)
+        H_dx = (Hx-H)/d_step# partial H/partial x
+        H_dy = (Hy-H)/d_step# partial H/partial y
 
+        #print(H)
         #e,n 
         v,w= np.linalg.eig(H) # eigenvalue and eigenstate
-        e=[]
-        if v[0]>v[1]:
-            e.extend([v[1],v[0]])
-            n=w[::-1]
+        #!!!!!!!!
+        #!!!!!!!!
+        if v[0].real>v[1].real:
+            e=[v[1],v[0]]
+            n=[w[:,1],w[:,0]]
         else:
-            e.extend([v[0],v[1]])
-            n=w
+            e=[v[0],v[1]]
+            n=[w[:,0],w[:,1]]
+
+        bc1= np.dot(np.dot(n[0],H_dx),n[1])*np.dot(np.dot(n[1],H_dy),n[0])/((e[0].real-e[1].real)**2)
+        bc2= np.dot(np.dot(n[1],H_dx),n[0])*np.dot(np.dot(n[0],H_dy),n[1])/((e[1].real-e[0].real)**2)
         
-        bc1= 1j*(np.dot(np.dot(n[0],H_dx),n[1].T)*np.dot(np.dot(n[1],H_dy),n[0].T)\
-            -np.dot(np.dot(n[0],H_dy),n[1].T)*np.dot(np.dot(n[1],H_dx),n[0].T))/((e[0]-e[1])**2)
-        bc2=1j*(np.dot(np.dot(n[1],H_dx),n[0].T)*np.dot(np.dot(n[0],H_dy),n[1].T)\
-            -np.dot(np.dot(n[1],H_dy),n[0].T)*np.dot(np.dot(n[0],H_dx),n[1].T))/((e[1]-e[0])**2)
-        
-        #print(bc1,bc2)        
+        # print(bc1,bc2)        
         # should be real number, drop numerical error 
-        berry_curv_1.append(bc1.real)
-        berry_curv_2.append(bc2.real)
-        berry_curv_total.append(bc1.real+bc2.real)
+        
+        berry_curv_1.append(bc1.imag)
+        berry_curv_2.append(bc2.imag)
+        berry_curv_total.append(bc1.imag+bc2.imag)
 
-
-
-    chern_number=get_berryphase(kxlist=kxlist,kylist=kylist,berry_curv=berry_curv_1) ##change which band to use!
+    chern_number=get_berryphase(kxlist=kxlist,kylist=kylist,berry_curv=berry_curv_1)/2/pi ##change which band to use!
+    print("chern_number = {}".format(chern_number))
 
     ## plot and save
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(6, 5)
     fig.subplots_adjust(wspace=0.4)
-    tick1=np.arange(-5,5.05,0.05)
-    tick2=np.arange(-5,5.05,0.05)
-    plt.yticks(tick2)
-    plt.xticks(tick1)
     ax.axis('off')
-
     im=ax.tricontourf(kxlist,kylist,berry_curv_1)  ##change which band to use!
     ax.set_aspect(1)
     #cax = add_right_cax(ax, pad=0.01, width=0.02)
@@ -121,4 +137,3 @@ def main(t1,t2,M,phi):
 
 if __name__=='__main__':
     main(t1=1,t2=0.05,M=0.,phi=pi/2)
-# %%
